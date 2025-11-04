af9d33cc7ab99a7be5c7c56334ebb5748c632c34
bbcritch <bbcritch@hotmail.com>
2025-11-04 13:25:24 -0700

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <h1 style={{textAlign:'center', padding:'100px', fontSize:'3rem', color:'#fff', background:'#111'}}>
+    HELLO WORLD
+  </h1>
+)
