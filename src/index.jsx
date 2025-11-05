59096c9d872652195054c9ab90b7dd9813a8c375
bbcritch <bbcritch@hotmail.com>
2025-11-04 17:18:14 -0700

    Delete src/index.jsx

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <h1 style={{textAlign:'center', padding:'100px', fontSize:'3rem', color:'#fff', background:'#111'}}>
-    HELLO WORLD
-  </h1>
-)
